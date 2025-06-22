3c03c8fb59a05ca55876c79a8b42fbbe18c06079
LeoStormer <nzeezenwa@gmail.com>
2025-06-22 02:13:54 -0400

    initial commit

---
+++ src/test/java/com/leostormer/strife/StrifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leostormer.strife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
