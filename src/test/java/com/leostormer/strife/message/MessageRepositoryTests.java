97291855739743b6cc3b6d7b929f44d7f3d90cff
LeoStormer <nzeezenwa@gmail.com>
2025-07-07 03:21:57 -0400

    write message repo tests part 1

--- src/test/java/com/leostormer/strife/message/MessageRepositoryTests.java
+++ src/test/java/com/leostormer/strife/message/MessageRepositoryTests.java
@@ -1,9 +1,12 @@
 package com.leostormer.strife.message;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -33,8 +36,8 @@ public class MessageRepositoryTests {
 
     @BeforeAll
     static void setupUsers(@Autowired UserRepository userRepository, @Autowired ConversationRepository conversationRepository) {
-        User[] users = new User[4];
-        for (int i = 0; i < 4; i++) {
+        User[] users = new User[3];
+        for (int i = 0; i < 3; i++) {
             User user = new User();
             user.setUsername("User" + (i + 1));
             int passwordDigits = 123 * (i + 1);
@@ -42,10 +45,10 @@ public class MessageRepositoryTests {
             users[i] = userRepository.save(user);
         }
         
-        for (int j = 0; j < users.length - 1; j++) {
-            Conversation conversation = new Conversation(users[j], users[j + 1], true, true);
-            conversationRepository.save(conversation);
-        }
+        Conversation convo1 = new Conversation(users[0], users[1], true, true);
+        Conversation convo2 = new Conversation(users[1], users[2], true , true);
+        Conversation convo3 = new Conversation(users[0], users[2], true, false);
+        conversationRepository.saveAll(List.of(convo1, convo2, convo3));
     }
 
     @AfterAll
@@ -56,16 +59,16 @@ public class MessageRepositoryTests {
 
     @BeforeEach
     void setup() {
-        User[] users = new User[4];
-        for (int i = 0; i < users.length; i++) {
-            users[i] = userRepository.findOneByUsername("User"+(i + 1)).get();
-        }
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
 
         for (int i = 0; i < 10; i++) {
             DirectMessage message = new DirectMessage();
+            message.setConversation(conversation);
+            message.setSender(i % 2 == 0 ? user1 : user2);
             message.setContent("This is a message! " + i);
-
-            message.setSender(i % 2 == 0 ? users[0] : users[1]);
+            messageRepository.save(message);
         }
     }
 
@@ -76,33 +79,60 @@ public class MessageRepositoryTests {
 
     @Test
     void shouldInsertDirectMessage() {
-        User user4 = userRepository.findOneByUsername("User4").get();
-        User user3 = userRepository.findOneByUsername("User3").get();
-        Conversation conversation = conversationRepository.findByUserIds(user4.getId(), user3.getId()).get();
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
         String messageContent = "AAAAAAHHHHHH!";
-        DirectMessage message = messageRepository.insertMessage(user4, conversation, messageContent);
+        DirectMessage message = messageRepository.insertMessage(user1, conversation, messageContent);
         assertNotNull(message.getId());
         assertTrue(message.getContent().equals(messageContent));
     }
 
     @Test
     void shouldFindDirectMessageById() {
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
+        DirectMessage message = messageRepository.insertMessage(user1, conversation, "This is a new message!");
 
+        Optional<DirectMessage> message2 = messageRepository.findDirectMessageById(message.getId());
+        assertTrue(message2.isPresent());
+        assertTrue(message2.get().getId().equals(message.getId()));
+        assertTrue(message2.get().getContent().equals(message.getContent()));
     }
 
     @Test
     void shouldGetDirectMessages() {
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
 
+        MessageSearchOptions searchOptions = MessageSearchOptions.builder().build();
+        List<DirectMessage> messages = messageRepository.getMessages(conversation, searchOptions);
+        assertTrue(messages.size() == 10);
+        assertTrue(messages.get(0).getContent().equals("This is a message! 0"));
     }
 
     @Test
     void shouldUpdateDirectMessage() {
-
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
+        DirectMessage message = messageRepository.insertMessage(user1, conversation, "This is a new message!");
+        DirectMessage updatedMessage = messageRepository.updateDirectMessage(message.getId(), "I have been updated");
+        assertTrue(message.getId().equals(updatedMessage.getId()));
+        assertFalse(message.getContent().equals(updatedMessage.getContent()));
     }
     
     @Test
     void shouldDeleteAllByConversation() {
-
+        User user1 = userRepository.findOneByUsername("User1").get();
+        User user2 = userRepository.findOneByUsername("User2").get();
+        Conversation conversation = conversationRepository.findByUserIds(user1.getId(), user2.getId()).get();
+        MessageSearchOptions searchOptions = MessageSearchOptions.builder().build();
+        assertTrue(messageRepository.getMessages(conversation, searchOptions).size() == 10);
+        messageRepository.deleteAllByConversation(conversation);
+        assertTrue(messageRepository.getMessages(conversation, searchOptions).size() == 0);
     }
 
     @Test
