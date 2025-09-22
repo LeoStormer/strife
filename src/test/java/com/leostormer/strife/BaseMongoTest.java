509b39f28762a3c14551363b82c8be3d8b2b3d56
LeoStormer <nzeezenwa@gmail.com>
2025-09-22 13:14:41 -0400

    Stop container from restarting between tests

--- src/test/java/com/leostormer/strife/BaseMongoTest.java
+++ src/test/java/com/leostormer/strife/BaseMongoTest.java
@@ -3,12 +3,8 @@ package com.leostormer.strife;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public class BaseMongoTest {
-    @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0");
 
     @DynamicPropertySource
