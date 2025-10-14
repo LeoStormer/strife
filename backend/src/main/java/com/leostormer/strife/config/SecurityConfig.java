f9348f7556a2cdc215735ec0463c524bb5a1efb5
LeoStormer <nzeezenwa@gmail.com>
2025-10-14 13:35:57 -0400

    configure cors and csrf protection for single page app

--- backend/src/main/java/com/leostormer/strife/config/SecurityConfig.java
+++ backend/src/main/java/com/leostormer/strife/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.leostormer.strife.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -11,22 +13,42 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 @EnableTransactionManagement
 public class SecurityConfig {
 
+    private CsrfTokenRepository csrfTokenRepository() {
+        CookieCsrfTokenRepository repository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity, not recommended for production
+                .cors(cors -> {
+                    cors.configurationSource(request -> {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                        configuration.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
+                        configuration.setAllowCredentials(true); // Allow sending cookies, needed for JSESSIONID
+                        return configuration;
+                    });
+                })
+                .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
+                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/user/register", "/login", "/logout", "/error").permitAll()
                         .anyRequest()
                         .authenticated())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form.usernameParameter("email").permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
