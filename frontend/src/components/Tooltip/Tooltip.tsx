a8a735005f08ca41eef5073ee157a396ef903725
LeoStormer <nzeezenwa@gmail.com>
2025-11-08 14:28:42 -0500

    make tooltip position adjustable

--- frontend/src/components/Tooltip/Tooltip.tsx
+++ frontend/src/components/Tooltip/Tooltip.tsx
@@ -1,49 +1,103 @@
-import { useEffect, useRef, useState, type RefObject } from 'react'
-import styles from "./ToolTip.module.css"
-import Modal from '../Modal'
-import StyleComposer from '../../utils/StyleComposer'
+import { useEffect, useRef, useState, type RefObject } from "react";
+import styles from "./ToolTip.module.css";
+import Modal from "../Modal";
+import StyleComposer from "../../utils/StyleComposer";
+
+type TailStyle = "up" | "down" | "left" | "right" | "none";
+type RenderDirection = Omit<TailStyle, "none">;
 
 type TooltipProps = {
   text: string;
   targetRef?: RefObject<HTMLElement>;
   tailStyle?: TailStyle;
-}
+  direction?: RenderDirection;
+};
 
-type TailStyle = "up" | "down" | "left" | "right" |"none"
-
+const TAIL_LENGTH = 4;
 const TAIL_MAP: Record<TailStyle, string | undefined> = {
   up: styles.up,
   down: styles.down,
   left: styles.left,
   right: styles.right,
   none: undefined,
-}
+};
 
-function Tooltip({ text, targetRef, tailStyle = 'left' }: TooltipProps) {
-  const [position, setPosition] = useState({top: 0, left: 0})
+const getTop = (
+  tailStyle: RenderDirection,
+  tooltipRect: DOMRect,
+  targetRect: DOMRect
+) => {
+  if (tailStyle === "right" || tailStyle === "left") {
+    return window.scrollY + targetRect.top;
+  }
 
-  useEffect(() =>{
-    if (!(targetRef && targetRef.current)) {
-      return
+  if (tailStyle === "up") {
+    return window.scrollY + targetRect.bottom + TAIL_LENGTH;
+  }
+
+  // tailstyle === bottom
+  return window.scrollY + targetRect.top - tooltipRect.height - TAIL_LENGTH;
+};
+
+const getLeft = (
+  tailStyle: RenderDirection,
+  tooltipRect: DOMRect,
+  targetRect: DOMRect
+) => {
+  if (tailStyle === "left") {
+    return window.scrollX + targetRect.right + TAIL_LENGTH;
+  }
+
+  if (tailStyle === "up" || tailStyle === "down") {
+    return (
+      window.scrollX +
+      targetRect.left +
+      targetRect.width / 2 -
+      tooltipRect.width / 2
+    );
+  }
+
+  //tailstyle == right
+  return window.scrollX + targetRect.left - tooltipRect.width - TAIL_LENGTH;
+};
+
+function Tooltip({
+  text,
+  targetRef,
+  tailStyle = "left",
+  direction = tailStyle === "none" ? "left" : tailStyle,
+}: TooltipProps) {
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  useEffect(() => {
+    if (!(tooltipRef && tooltipRef.current && targetRef && targetRef.current)) {
+      return;
     }
 
     const targetRect = targetRef.current.getBoundingClientRect();
-    const top = targetRect.top
-    const left = targetRect.left + targetRect.width + 4
-    setPosition({top: window.screenY + top, left: window.screenX + left})
-  }, [ text])
+    const tooltipRect = tooltipRef.current.getBoundingClientRect();
+    const top = getTop(direction, tooltipRect, targetRect);
+    const left = getLeft(direction, tooltipRect, targetRect);
+
+    setPosition({ top: top, left: left });
+  }, [text, tailStyle, direction]);
 
   const bubbleClass = StyleComposer(styles.bubble, {
     [TAIL_MAP[tailStyle] as string]: true,
-  })
+  });
 
   return (
-    <Modal className={styles.container} style={{position:'absolute', top: position.top, left:position.left}}>
+    <Modal
+      ref={tooltipRef}
+      className={styles.container}
+      style={{ position: "absolute", top: position.top, left: position.left }}
+    >
       <div className={bubbleClass}>
         <p className={styles.content}>{text}</p>
       </div>
     </Modal>
-  )
+  );
 }
 
-export default Tooltip
+export default Tooltip;
