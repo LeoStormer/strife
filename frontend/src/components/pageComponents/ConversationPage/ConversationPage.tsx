19e302c558ee35b063012e3e08c389e5b5fe640b
LeoStormer <nzeezenwa@gmail.com>
2025-11-22 12:21:15 -0500

    wireframe ConversationPage

---
+++ frontend/src/components/pageComponents/ConversationPage/ConversationPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./ConversationPage.module.css";
+import sharedStyles from "../../../styles/ChannelViewer.module.css";
+
+function ConversationPage() {
+  return (
+    <div className={sharedStyles.container}>
+      <div className={sharedStyles.channelTitleBarContainer}>UserName</div>
+      <div className={sharedStyles.flexContainer}>
+        <div className={sharedStyles.messageViewAreaContainer}>
+          Message View Area
+          <div className={sharedStyles.messageInputContainer}>
+            Message #channel-name
+          </div>
+        </div>
+        <div className={styles.profileContainer}>User Profile</div>
+      </div>
+    </div>
+  );
+}
+
+export default ConversationPage;
