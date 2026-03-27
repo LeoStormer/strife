87970b610e859e3849630616b5e87c3229fd1a6b
LeoStormer <nzeezenwa@gmail.com>
2026-03-27 14:45:47 -0400

    style empty activity feed

--- frontend/src/components/pageComponents/FriendsPage/ActivityFeed/ActivityFeed.tsx
+++ frontend/src/components/pageComponents/FriendsPage/ActivityFeed/ActivityFeed.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import styles from "./ActivityFeed.module.css"
+import React from "react";
+import styles from "./ActivityFeed.module.css";
 
 function ActivityFeed() {
   return (
-    <div className={styles.container}>ActivityFeed</div>
-  )
+    <div className={styles.container}>
+      <h2 id='side-panel-title'>Active Now</h2>
+      <div className={styles.emptyCard}>
+        <h3>It's quiet for now...</h3>
+        <p>
+          When a friend starts an activity—like playing a game or hanging out on
+          voice—we'll show it here!
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default ActivityFeed
+export default ActivityFeed;
