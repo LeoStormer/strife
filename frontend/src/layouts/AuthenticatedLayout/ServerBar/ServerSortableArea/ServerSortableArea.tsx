f5f7fc130a9f34a1de2c7ce535c6918e5ee753cd
LeoStormer <nzeezenwa@gmail.com>
2026-03-27 21:55:31 -0400

    refactor for readability

--- frontend/src/layouts/AuthenticatedLayout/ServerBar/ServerSortableArea/ServerSortableArea.tsx
+++ frontend/src/layouts/AuthenticatedLayout/ServerBar/ServerSortableArea/ServerSortableArea.tsx
@@ -16,29 +16,25 @@ import {
   type KeyboardCoordinateGetter,
 } from "@dnd-kit/core";
 import { snapCenterToCursor } from "@dnd-kit/modifiers";
-import Draggable, {
-  type TransformOverride,
-} from "../../../components/dragndrop/Draggable";
-import Droppable from "../../../components/dragndrop/Droppable";
-import Portal from "../../../components/Portal";
-import Icon from "../../../components/Icon";
-import ServerIcon from "../../../components/ServerIcon";
+import Portal from "../../../../components/Portal";
+import ServerIcon from "../../../../components/ServerIcon";
 import {
   useServerSelectionContext,
   type Folder,
   type Server,
   type ServerItem,
-} from "../../../contexts/ServerSelectionContext";
-import StyleComposer from "../../../utils/StyleComposer";
-import TooltipTrigger from "../../../components/TooltipTrigger";
-import { Link } from "react-router-dom";
-import styles from "./ServerBar.module.css";
-import type { ServerItemOrFolderRecord } from "../../../contexts/ServerSelectionContext/serverReducer";
+} from "../../../../contexts/ServerSelectionContext";
+import styles from "../ServerBar.module.css";
+import type { ServerItemOrFolderRecord } from "../../../../contexts/ServerSelectionContext/serverReducer";
+import {
+  Mover,
+  ServerFolder,
+  ServerIconDisplayGrid,
+  ServerListItem,
+  type DragType,
+} from "./ServerSortableComponents";
 
-const restrictSortableToOriginalPosition: TransformOverride = (transform) => {
-  void transform;
-  return { transform: undefined };
-};
+// TODO: Migrate from legacy dnd-kit to latest dnd-kit/react
 
 const hybridCollision: CollisionDetection = (args) => {
   if (args.pointerCoordinates) {
@@ -48,215 +44,11 @@ const hybridCollision: CollisionDetection = (args) => {
   return closestCenter(args);
 };
 
-type DragType = "server" | "folder" | null;
-
 type DragNDropData = {
   source: "last" | string;
   type?: "mover" | "combiner";
   folderId?: string | undefined;
 };
-
-type MoverProps = {
-  moverId: string;
-  dragType: DragType;
-  sourceId?: string | undefined;
-  isDragging?: boolean;
-  folderId?: string | undefined;
-  isEnabled?: boolean;
-};
-
-function Mover({
-  moverId,
-  sourceId,
-  isDragging = false,
-  dragType,
-  folderId,
-  isEnabled = true,
-}: MoverProps) {
-  return (
-    <Droppable
-      className={StyleComposer(styles.droppable, {
-        [styles.dragging as string]: isDragging,
-        [styles.notAllowed as string]:
-          dragType === "folder" && folderId != undefined,
-      })}
-      id={`Mover(${moverId})`}
-      disabled={!isEnabled}
-      data={{ source: sourceId, type: "mover", folderId }}
-    />
-  );
-}
-
-type ServerListItemProps = {
-  server: Server;
-  selectedServerId: string | null;
-  draggingId: string | null;
-  dragType: DragType;
-  folderId?: string | undefined;
-  isPillHidden?: boolean;
-  isDNDEnabled?: boolean;
-};
-
-function ServerListItem({
-  server,
-  selectedServerId,
-  draggingId,
-  dragType,
-  folderId,
-  isPillHidden = false,
-  isDNDEnabled = true,
-}: ServerListItemProps) {
-  const { id, name, icon } = server;
-  const { getTargetProps, getTriggerProps } = TooltipTrigger<HTMLLIElement>({
-    tooltipText: name,
-    tailStyle: "left",
-  });
-
-  return (
-    <li {...getTargetProps()} className={styles.listItem}>
-      <Draggable
-        id={id}
-        transformOverride={restrictSortableToOriginalPosition}
-        disabled={!isDNDEnabled}
-        data={{ source: id, folderId }}
-        className={styles.draggable}
-      >
-        <Link
-          {...getTriggerProps()}
-          tabIndex={0}
-          to={`/servers/${id}`}
-          className={StyleComposer(styles.navItem, {
-            [styles.selected as string]: selectedServerId === id,
-            [styles.pillHidden as string]: isPillHidden,
-          })}
-        >
-          <ServerIcon serverName={name} serverIconImage={icon} />
-        </Link>
-      </Draggable>
-      <Mover
-        moverId={id}
-        sourceId={id}
-        isDragging={draggingId === id}
-        dragType={dragType}
-        folderId={folderId}
-        isEnabled={isDNDEnabled}
-      />
-      <Droppable
-        className={StyleComposer(`${styles.droppable} ${styles.combiner}`, {
-          [styles.dragging as string]: draggingId === id,
-          [styles.notAllowed as string]: dragType === "folder",
-        })}
-        id={`Combiner(${id})`}
-        data={{ source: id, type: "combiner", folderId }}
-        disabled={!isDNDEnabled}
-      />
-    </li>
-  );
-}
-
-function ServerIconDisplayGrid({ servers }: { servers: Server[] }) {
-  return (
-    <div className={styles.iconDisplay}>
-      {servers.map(({ name, icon, id }) => (
-        <ServerIcon
-          className={styles.icon}
-          serverIconImage={icon}
-          serverName={name}
-          key={`icon-${id}`}
-        />
-      ))}
-    </div>
-  );
-}
-
-type ServerFolderProps = Omit<ServerListItemProps, "server" | "folderId"> & {
-  servers: Server[];
-  id: string;
-};
-
-function ServerFolder({
-  id,
-  servers,
-  selectedServerId,
-  draggingId,
-  dragType,
-  isDNDEnabled = true,
-}: ServerFolderProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const names = servers.map((s) => s.name).reduce((s1, s2) => `${s1} | ${s2}`);
-  const { getTargetProps, getTriggerProps } = TooltipTrigger<HTMLLIElement>({
-    tooltipText: names,
-    tailStyle: "left",
-  });
-
-  const folderClass = StyleComposer(`${styles.folder} ${styles.list}`, {
-    [styles.open as string]: isOpen,
-    [styles.pillHidden as string]: isOpen,
-  });
-
-  return (
-    <li {...getTargetProps()} className={styles.listItem}>
-      <Mover
-        moverId={id}
-        sourceId={id}
-        dragType={dragType}
-        isEnabled={isDNDEnabled}
-        isDragging={draggingId === id}
-      />
-      <Droppable
-        className={StyleComposer(`${styles.droppable} ${styles.combiner}`, {
-          [styles.dragging as string]: draggingId === id,
-          [styles.notAllowed as string]: dragType === "folder",
-        })}
-        id={`Combiner(${id})`}
-        data={{ source: "last", type: "combiner", folderId: id }}
-        disabled={!isDNDEnabled}
-      />
-      <Draggable
-        id={id}
-        data={{ source: id }}
-        transformOverride={restrictSortableToOriginalPosition}
-        disabled={!isDNDEnabled}
-        className={styles.draggable}
-      >
-        <ul
-          {...(!isOpen ? getTriggerProps() : {})}
-          className={folderClass}
-          onClick={() => {
-            if (!isOpen) {
-              setIsOpen(true);
-            }
-          }}
-        >
-          <div className={styles.wrapper}>
-            <li className={styles.listItem}>
-              <button
-                {...getTriggerProps()}
-                className={styles.folderButton}
-                onClick={() => setIsOpen(false)}
-              >
-                <Icon name='folder-fill' />
-              </button>
-              <ServerIconDisplayGrid servers={servers} />
-            </li>
-            {servers.map((server, i) => (
-              <ServerListItem
-                folderId={id}
-                selectedServerId={selectedServerId}
-                server={server}
-                draggingId={draggingId}
-                dragType={dragType}
-                isPillHidden={!isOpen}
-                isDNDEnabled={isOpen}
-                key={server.id}
-              />
-            ))}
-          </div>
-        </ul>
-      </Draggable>
-    </li>
-  );
-}
 
 type KeyboardNode = {
   id: string;
@@ -355,6 +147,15 @@ function ServerSortableArea() {
     event,
     { context },
   ) => {
+    // TODO: filter graph to be aware of folder open state
+    // - Closed folders should have their children removed from the graph
+    // - Arrows up and down should skip the children of closed folders
+    // - If shift is pressed draggable should only see 'combiners'
+    // otherwise only see 'movers'
+    // - Arrow right should enter a folder, opening it and allowing
+    // its children to be dragged over.
+    // - Arrow left should exit a folder, closing it and causing its children
+    // to be skipped
     const { draggingNodeRect, droppableRects } = context;
     const graph = graphRef.current;
     const currentIndex = keyboardIndexRef.current;
@@ -441,6 +242,15 @@ function ServerSortableArea() {
     useSensor(TouchSensor),
     useSensor(KeyboardSensor, {
       coordinateGetter: customKeyboardCoordinates,
+      keyboardCodes: {
+        start: ['Space'],
+        end: ['Space'],
+        cancel: ['Escape'],
+        up: ['ArrowUp'],
+        down: ['ArrowDown'],
+        left: ['ArrowLeft'],
+        right: ['ArrowRight'],
+      },
     }),
   );
 
